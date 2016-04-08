97a75a8fdc1f4a1c569d981a5f544f41804d268d
zyouhua <zyouhua@gmail.com>
2016-04-09 07:29:21 +0800

    u

--- codeinit.bat
+++ codeinit.bat
@@ -4,4 +4,7 @@ if not exist %USERPROFILE%\.ssh (
 )
 copy /Y c:\unseen\id_rsa %USERPROFILE%\.ssh\id_rsa
 cd ..
+git clone git@github.com:aiyou4love/unseen.git
+git clone git@github.com:aiyou4love/autopack.git
+git clone git@github.com:aiyou4love/webui.git
 @echo on
